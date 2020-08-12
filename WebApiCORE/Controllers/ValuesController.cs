ec6e7712e170bdc45e7ba98c801cc2f92361790b
guancopper <1229865681@qq.com>
2020-08-12 23:29:01 +0800

    post传入

--- WebApiCORE/Controllers/ValuesController.cs
+++ WebApiCORE/Controllers/ValuesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,14 +9,17 @@ using WebApiCORE.Mode;
 using WebAPICore.Struct;
 using WebApICore.Base;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
 
 namespace WebApiCORE.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         //[Route("Users")]
+        [HttpGet]
         public IActionResult GetSnapshot(string tagname)
         {
             //string[] tag_list = { "lua.fuzhuceshi" };
@@ -26,6 +30,16 @@ namespace WebApiCORE.Controllers
             //value.pass = "golden";
             var res = JToken.FromObject(lists).ToString();
             return Ok(res);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult PutSnapshot([FromBody] object obj)
+        {
+            //var str = "hello";
+            var DynamicObject = JsonConvert.SerializeObject(obj);
+            //string[] tag_list = tagname.Split(',');
+            //object lists = SnapShotManager.GetSnapShot(tag_list);
+            return Ok(DynamicObject);
         }
     }
 }
---
+++ WebApiCORE/Values.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCORE
+{
+    public class Values
+    {
+        public string TagName { get; set; }
+    }
+}
